9de34bf85b3eee0af8a18377b13a9e71cb6ce27b
MichelAngelo <baksdevelopmen@gmail.com>
2026-03-02 02:17:28 +0300

    BaksDevelopment

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -31,7 +31,8 @@ return static function(ContainerConfigurator $configurator) {
     $services = $configurator->services()
         ->defaults()
         ->autowire()
-        ->autoconfigure();
+        ->autoconfigure()
+        ->public();
 
     $NAMESPACE = BaksDevProductsPromotionBundle::NAMESPACE;
     $PATH = BaksDevProductsPromotionBundle::PATH;
